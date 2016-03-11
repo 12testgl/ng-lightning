7b80819ee49bd21f56b15b9c85393d3f2af5b86e
Spyridon Alfredos Desyllas <sadesyllas@gmail.com>
2016-03-11 16:25:08 +0200

    chore(build): use karma, saucelabs and travis for continuous integration

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,33 @@
+const fs = require('fs');
+
+// Use ENV vars on Travis and sauce.json locally to get credentials
+if (!process.env.SAUCE_USERNAME) {
+  if (!fs.existsSync('./sauce.json')) {
+    console.log('Create a sauce.json with your SauceLabs credentials.');
+    process.exit(1);
+  } else {
+    var credentials = require('./sauce');
+    process.env.SAUCE_USERNAME = credentials.username;
+    process.env.SAUCE_ACCESS_KEY = credentials.accessKey;
+  }
+}
+
+const launchers = {
+  'SL_Chrome': {
+    base: 'SauceLabs',
+    browserName: 'chrome'
+  },
+  // 'SL_InternetExplorer': {
+  //   base: 'SauceLabs',
+  //   browserName: 'internet explorer',
+  //   version: '11'
+  // },
+  'SL_FireFox': {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+  }
+};
+
 module.exports = function(config) {
   config.set({
 
@@ -33,18 +63,20 @@ module.exports = function(config) {
 
     preprocessors: {'temp/**/*.js': ['sourcemap']},
 
-    reporters: process.env.TRAVIS ? ['dots'] : ['progress'],
+    reporters: ['dots', 'saucelabs'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
+    browsers: process.env.TRAVIS ? Object.keys(launchers) : ['Chrome'],
     autoWatch: true,
-    browsers: process.env.TRAVIS ? ['Chrome_travis_ci', 'Firefox'] : ['Chrome'],
     singleRun: false,
-    customLaunchers: {
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
+    captureTimeout: 120000,
+    customLaunchers: launchers,
+    sauceLabs: {
+      connectOptions: {
+        logfile: './karma-saucelabs.log',
+      },
+      tunnelIdentifier: 'ng-lightning',
+    }
   });
 };
