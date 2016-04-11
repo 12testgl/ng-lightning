4f58f0e7ee18159a5cff163a9234818aa57d7dd4
Spyridon Alfredos Desyllas <sadesyllas@gmail.com>
2016-04-11 11:26:50 +0300

    fix(app): provide temporary fix for angular #7721
    Substitute angular2-polyfills package as per https://github.com/angular/angular/issues/7721

--- demo/vendor.ts
+++ demo/vendor.ts
@@ -1,5 +1,9 @@
 import 'es6-shim/es6-shim.js';
-import 'angular2/bundles/angular2-polyfills.js';
+/* START -- TEMPORARY FIX FOR https://github.com/angular/angular/issues/7721 */
+// import 'angular2/bundles/angular2-polyfills.js';
+import 'reflect-metadata/Reflect.js';
+import 'zone.js/dist/zone.js';
+/* END -- TEMPORARY FIX FOR https://github.com/angular/angular/issues/7721 */
 
 // Angular 2
 import 'angular2/platform/browser';
