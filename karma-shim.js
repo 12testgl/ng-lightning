356bd170e4040b22af9808bdd7a3717675531a9f
Kostas Galanopoulos <galanopok@gmail.com>
2017-08-25 22:25:41 +0300

    feat(modals, notifications): Add animation support

--- karma-shim.js
+++ karma-shim.js
@@ -23,7 +23,10 @@ System.config({
     '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
     '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
     '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-
+    '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
+    '@angular/animations/browser': 'npm:@angular/animations/bundles/animations-browser.umd.js',
+    '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
+    
     // angular testing umd bundles
     '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
     '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
