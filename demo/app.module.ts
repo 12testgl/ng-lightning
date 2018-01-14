8ac0617cc36804b410edb583f8369bfad63bde9b
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2018-01-14 23:30:19 +0200

    chore(app): upgrade angular & lds

--- demo/app.module.ts
+++ demo/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import {RouterModule, PreloadAllModules} from '@angular/router';
 import {routes} from './app.routing';
@@ -12,6 +13,7 @@ import {AppComp} from './app';
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes, { useHash: true, preloadingStrategy: PreloadAllModules }),
     NglModule.forRoot(),
   ],
