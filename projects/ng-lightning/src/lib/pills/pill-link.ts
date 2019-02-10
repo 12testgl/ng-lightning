51f7e0da8dfb05c44d47123baeceea39ff3f2dc7
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2019-02-10 17:11:49 +0200

    feat(pills): migrate to LDS v2.8
    BREAKING CHANGE:
      * `[NglPillImage]` selector is not needed anymore
      * `(nglPillRemove)` output has been renamed to `remove`
      * `[nglPillRemovable]` input has been renamed to `removable`
    
      Before:
    
      ```html
      <ngl-pill (nglPillRemove)="remove()" NglPillRemovable="false">
        <ngl-icon iconName="standard:feedback" nglPillImage></ngl-icon>
      </ngl-pill>
      ```
    
      After:
    
      ```html
      <ngl-pill icon="standard:feedback" (remove)="remove()" removable="false"></ngl-pill>
      ```
    
    
    ADDED
      * `[icon]`: NglIcon component or iconName to show on the left of the pill
      * `[avatar]`: NglAvatar component or src to show on the left of the pill
      * `[hasError]`: Applies the error style to the component
      * `[removeTitle]`: Remove button title (and assistive text)

--- projects/ng-lightning/src/lib/pills/pill-link.ts
+++ projects/ng-lightning/src/lib/pills/pill-link.ts
@@ -1,17 +1,22 @@
-import {Directive, ElementRef, Renderer2, Optional} from '@angular/core';
-import {NglPill} from './pill';
+import { Component, Optional, ChangeDetectionStrategy, ElementRef, Renderer2 } from '@angular/core';
+import { NglPill } from './pill';
 
-@Directive({
-  // tslint:disable-next-line:directive-selector
+@Component({
+  //  tslint:disable-next-line:component-selector
   selector: 'a',
+  template: `
+    <span class="slds-pill__label">
+      <ng-content></ng-content>
+    </span>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NglPillLink {
-
   constructor(@Optional() pill: NglPill, element: ElementRef, renderer: Renderer2) {
     if (!pill) { return; }
 
-    renderer.addClass(element.nativeElement, 'slds-pill__label');
-    pill.unlinked = false;
+    renderer.addClass(element.nativeElement, 'slds-pill__action');
+    pill.linked = true;
   }
 
 }
